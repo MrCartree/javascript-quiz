d4788461315bbb62ffe73b09ed481fca395ba026
Zach LaFleur <zach.lafleur95@gmail.com>
2020-08-15 17:03:47 -0600

    changed format of questions to be an array like object as well as changed a hide function. altered variables too

--- script.js
+++ script.js
@@ -1,43 +1,58 @@
 // declaring global variables
-let startBtn = document.getElementById("beginBtn");
-let timeTxt = document.getElementById("time");
-let questionSpot = document.getElementById("questionSpot");
-let answerA = document.getElementById("answerA")
-let answerB = document.getElementById("answerB")
-let answerC = document.getElementById("answerC")
-let answerD = document.getElementById("answerD")
+let startBtn = document.querySelector("#beginBtn");
+let timeTxt = document.querySelector("#time");
 
 // objects for storing questions and answers
-let questions = {
-    question1: "What is 2 + 2?",
-    question2: "",
-    question3: "",
-    question4: "",
-    question5: ""
-};
 
-let answer1 = {
-    answerA: "4",
-    answerB: "11",
-    answerC: "5",
-    answerD: "9",
-};
+let qAndA = [
+    {
+        question: "2 + 2 = ?",
+        answer: ["4", "10", "5", "yes"],
+        correctA: "4"
+    },
 
-// Repopulation of internal stuff
-function repop() {
-    questionSpot.textContent = questions.question1;
-    answerA.textContent = answer1.answerA;
-    answerB.textContent = answer1.answerB;
-    answerC.textContent = answer1.answerC;
-    answerD.textContent = answer1.answerD;
-};
+    {
+        question: "How are you?",
+        answer: ["A", "B", "C", "D"],
+        correctA: "C"
+    },
 
+    {
+        question: "This assignment is hard?",
+        answer: ["A", "B", "C", "D"],
+        correctA: "B"
+    },
+
+    {
+        question: "question4",
+        answer: ["A", "B", "C", "D"],
+        correctA: "D"
+    },
+
+    {
+        question: "This is the final question?",
+        answer: ["A", "B", "C", "D"],
+        correctA: "A"
+    }
+];
+
+// Element creation/appending
+// let createQ = document.createElement("h5");
+// let createUl = document.createElement("ul");
+// createQ.append("#cardBody")
+
+// displays and appends content
+function display() {
+    for (let i = 0; i < qAndA.length; i++) {
+        let userQuestion = qAndA[i].question
+        let userAnswer = qAndA[i].answer
+        createQ.textContent = userQuestion
+    }
+}
 
 // CountDown timer
 let secondsLeft = 60;
-
 function timerStart() {
-    startBtn.className = "hide";
     let timeHandler = setInterval(function() {
         timeTxt.innerText = "Seconds left: " + secondsLeft;
         secondsLeft--
@@ -48,8 +63,25 @@ function timerStart() {
 
 };
 
+// Hide function
+let questionTxt = document.querySelector(".questionTxt");
+let pTxt1 = document.querySelector(".pTxt1");
+let pTxt2 = document.querySelector(".pTxt2");
+let pTxt3 = document.querySelector(".pTxt3");
+let pTxt4 = document.querySelector(".pTxt4");
+
+function hide() {
+    questionTxt.className = "hide";
+    pTxt1.className = "hide";
+    pTxt2.className = "hide";
+    pTxt3.className = "hide";
+    pTxt4.className = "hide";
+    startBtn.className = "hide";
+};
+
 // Starts the Timer
 startBtn.addEventListener("click", function() {
     timerStart();
-    repop();
+    // hide();
+    display();
 });
